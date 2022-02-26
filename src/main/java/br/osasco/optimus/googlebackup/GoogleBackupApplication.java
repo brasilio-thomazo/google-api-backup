ae8c7dc2085239d70af59c6b668655638c3ea8f2
Brasilio Thomazo] <darkxsp@hotmail.com>
2022-02-26 02:47:10 -0300

    Criação do projeto e configuração de conexão com DB

---
+++ src/main/java/br/osasco/optimus/googlebackup/GoogleBackupApplication.java
@@ -0,0 +1,13 @@
+package br.osasco.optimus.googlebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleBackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleBackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/osasco/optimus/googlebackup/GoogleBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.osasco.optimus.googlebackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleBackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
